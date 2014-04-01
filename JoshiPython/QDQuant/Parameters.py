b7caeee5918347c1aefca0965d0264576d40ae86
GBelzoni <patrickhcostello@gmail.com>
2014-04-01 22:25:22 +0300

    Parameters constant and piecwise constant working

--- JoshiPython/QDQuant/Parameters.py
+++ JoshiPython/QDQuant/Parameters.py
@@ -4,6 +4,8 @@ Created on Feb 21, 2014
 @author: phcostello
 '''
 
+from scipy.interpolate import interp1d
+from bisect import bisect_left
 
 class Parameters(object):
     '''
@@ -37,6 +39,7 @@ class ParametersConstant(object):
         '''
     
         self.constant = constant
+        self.constantSqr = constant**2
         
     def int(self, t1, t2):
         
@@ -45,5 +48,101 @@ class ParametersConstant(object):
     
     def int_sqr(self, t1, t2):
         
-        return self.constant**2/2.*(t2-t1)
+        return self.constantSqr*(t2-t1)
+
+class ParametersPWConstant(object):
+    '''
+    Uses python bisect class to efficiently look up index for time points
+    '''
+
+    def __init__(self, x, y):
+        '''
+        Inputs:
+        x: time poinst
+        y: are constants where y[i] is constant over interval [x[i],x[i+1])
+        
+        Currently not supporting extrapolation beyond endpoints
+        '''
+    
+        self.x = x
+        self.y = y
+        
+    def int(self, t1, t2):
+        
+        '''
+        Does piecwise integral between t1 and t2
+        '''
+        
+        front_index = bisect_left(self.x, t1) 
+        back_index = bisect_left(self.x, t2 )
+        
+        full_interval_integration = [self.y[i]*(self.x[i+1]-self.x[i]) for i in range(front_index, back_index-1)]
+        front_stub = self.y[front_index-1]*(self.x[front_index] - t1) 
+        back_stub = self.y[back_index-1]*(t2- self.x[back_index -1])
+        
+        return front_stub + sum(full_interval_integration) + back_stub
+    
+    def int_sqr(self, t1, t2):
+       
+        '''
+        Does piecwise integral of square between t1 and t2
+        ''' 
+        front_index = bisect_left(self.x, t1) 
+        back_index = bisect_left(self.x, t2 )
+        
+        full_interval_integration = [(self.y[i]**2)*(self.x[i+1]-self.x[i]) for i in range(front_index, back_index-1)]
+        front_stub = (self.y[front_index-1]**2)*(self.x[front_index] - t1) 
+        back_stub = (self.y[back_index-1]**2)*(t2- self.x[back_index -1])
+        
+        return front_stub + sum(full_interval_integration) + back_stub
+        
+        
+    
+if __name__ == "__main__":
+    
+    #uses scipy interpolators
+    import numpy as np
+    x = np.array([0.0,1.0,3.,7.])
+    
+    y = np.array([1.,2.,3.,4.])
+    t1 = 0.
+    t2 = 1.4
+    t3 = 5.6
+    pw_const = ParametersPWConstant(x,y)
+    integral1 = pw_const.int(t1,t2)
+    integral2 = pw_const.int(t1,t3)
+    integral3 = pw_const.int(t2,t3)
+    
+    #Manually calc integrals by hand
+    calc1 = 1*1. + 0.4*2. 
+    calc2 = 1*1. + 2.0*2. + 2.6*3.
+    calc3 = 2.0*1.6 + 3.0*2.6
+    print integral1, calc1
+    print integral2, calc2
+    print integral3, calc3
+   
+    #Do piecwise constants 
+    integral1 = pw_const.int_sqr(t1,t2)
+    integral2 = pw_const.int_sqr(t1,t3)
+    integral3 = pw_const.int_sqr(t2,t3)
+    
+    #Manually calc integrals by hand
+    calc1 = 1*1. + 0.4*2**2. 
+    calc2 = 1*1. + 2.0*2**2. + 2.6*3**2.
+    calc3 = 2.0**2*1.6 + 3.0**2*2.6
+        
+    print integral1, calc1
+    print integral2, calc2
+    print integral3, calc3
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
--- JoshiPython/QDQuant/tests/ParametersTest.py
+++ JoshiPython/QDQuant/tests/ParametersTest.py
@@ -3,8 +3,11 @@ Created on Mar 15, 2014
 
 @author: phcostello
 '''
+
 import unittest
-from Parameters import ParametersConstant
+from Parameters import ParametersConstant, ParametersPWConstant
+
+import numpy as np
 
 class Test(unittest.TestCase):
 
@@ -26,18 +29,53 @@ class Test(unittest.TestCase):
         t1 = 0.1
         t2 = 0.4
         const_par = ParametersConstant(const_val)
-        integral = const_par.int(t1,t2)
-        calc = const_val*(t2-t1)
+        integral = const_par.int_sqr(t1,t2)
+        calc = const_val**2*(t2-t1)
         self.assertAlmostEqual(calc, integral)
 
 
     def test_piecewise_constant_integral(self):
-        pass
-    
+        
+        
+        x = np.array([0,1,3,7])
+        y = np.array([1,2,3,4])
+        t1 = 0.
+        t2 = 1.4
+        t3 = 5.6
+        pw_const = ParametersPWConstant(x,y)
+        integral1 = pw_const.int(t1,t2)
+        integral2 = pw_const.int(t1,t3)
+        integral3 = pw_const.int(t2,t3)
+        #Manually calc integrals by hand
+        calc1 = 1*1. + 0.4*2. 
+        calc2 = 1*1. + 2.0*2. + 2.6*3.
+        calc3 = 2.0*1.6 + 3.0*2.6
+        self.assertAlmostEqual(first=calc1, second=integral1)
+        self.assertAlmostEqual(first=calc2, second=integral2)
+        self.assertAlmostEqual(first=calc3, second=integral3)
+        
+        
     def test_piecewise_constant_integral_squared(self):
-        pass
 
-
+        
+        x = np.array([0,1,3,7])
+        y = np.array([1,2,3,4])
+        t1 = 0.
+        t2 = 1.4
+        t3 = 5.6
+        pw_const = ParametersPWConstant(x,y)
+        integral1 = pw_const.int_sqr(t1,t2)
+        integral2 = pw_const.int_sqr(t1,t3)
+        integral3 = pw_const.int_sqr(t2,t3)
+        #Manually calc integrals by hand
+        calc1 = 1*1. + 0.4*2**2. 
+        calc2 = 1*1. + 2.0*2**2. + 2.6*3**2.
+        calc3 = 2.0**2*1.6 + 3.0**2*2.6
+        self.assertAlmostEqual(first=calc1, second=integral1)
+        self.assertAlmostEqual(first=calc2, second=integral2)
+        self.assertAlmostEqual(first=calc3, second=integral3)
+        
+ 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
